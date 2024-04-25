3886de9b8f18629c2bf592e7222c0d6495753507
Michael Ingram <michaelingram093@gmail.com>
2024-04-25 10:52:11 -0400

    about section updates

--- gatsby/src/pages/about.js
+++ gatsby/src/pages/about.js
@@ -11,12 +11,12 @@ export default function AboutPage() {
           <h1 className="content-title">About Us</h1>
           <div className="scrollable-content">
             <p className="about-info">
-              Green Lake Investments is an Iowa based property management
-              company. Founded in 2022 and Co-Owned by Chris Ingram and Margaret Ingram.
-              Chris Ingram brings 30 years of experience in management and insurance. Margaret Ingram
-              is a Certified Public Account with 30 years of experience in accounting and finances. Together
-              they run Green Lake Investments which provides property rentals in the Des Moines, Iowa and
-              Lake Ozark, Missouri regions.
+              Green Lake Investments is an Iowa based property management company.
+              Founded in 2022, the owners bring extensive experience from property management, finance
+              and insurance industries. Green Lake Investments is focused on providing a great long term rental
+              experience to the Missouri and Iowa regions. Specifically, focusing on a small number of properties
+              in the Lake of the Ozarks and Des Moines Regions, Green Lake Investments aims to provide a personal,
+              family run business experience to its tenants. For more information, please contact us!
             </p>
           </div>
         </div>
